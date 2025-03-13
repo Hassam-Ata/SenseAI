7b8af6af1f1611916a81462d17bc653d8c87c15c
Hassam-Ata <hassamata86@gmail.com>
2025-03-14 04:12:24 +0500

    done something

--- app/lib/schema.ts
+++ app/lib/schema.ts
@@ -1,3 +1,4 @@
+import { Organization } from "@clerk/nextjs/server";
 import { z } from "zod";
 export const onboardingSchema = z.object({
   industry: z.string({ required_error: "Please select an industry" }),
@@ -22,3 +23,46 @@ export const onboardingSchema = z.object({
   ),
 });
 export type OnboardingSchemaType = z.infer<typeof onboardingSchema>;
+
+export const contactSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  mobile: z.string().optional(),
+  linkedin: z.string().optional(),
+  twitter: z.string().optional(),
+});
+export type ContactSchemaType = z.infer<typeof contactSchema>;
+
+export const entrySchema = z
+  .object({
+    title: z.string().min(1, "Title is required"),
+    organization: z.string().min(1, "Organization is required"),
+    startDate: z.string().min(1, "Start date is required"),
+    endDate: z.string().optional(),
+    description: z.string().min(1, "Description is required"),
+    current: z.boolean().default(false),
+  })
+  .refine(
+    (data) => {
+      if (!data.current && !data.endDate) {
+        return false;
+      }
+      return true;
+    },
+    {
+      message: "End date is required unlees this is your current position",
+      path: ["endDate"],
+    }
+  );
+
+export type EntrySchemaType = z.infer<typeof entrySchema>;
+
+export const resumeSchema = z.object({
+  contactInfo: contactSchema,
+  summary: z.string().min(1, "Professional summary is required"),
+  skills: z.string().min(1, "Skills are required"),
+  experience: z.array(entrySchema),
+  education: z.array(entrySchema),
+  projects: z.array(entrySchema),
+});
+
+export type ResumeSchemaType = z.infer<typeof resumeSchema>;
